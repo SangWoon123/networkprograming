ee40cfbddc34040790f14601d474c6e4d07b4828
namtaeu <113846111+namtaeu@users.noreply.github.com>
2023-06-05 05:08:18 +0900

    Update SharedArea.java
    findtour모듈 공유데이터 저장변수 추가

--- server/src/server/SharedArea.java
+++ server/src/server/SharedArea.java
@@ -12,6 +12,10 @@ public class SharedArea {
     public static Socket bookmark_client_socket;
     public static String tobookmark_msg="즐겨찾기 모듈로 전달할 메시지가 저장되지 않았습니다"; //클라이언트가 즐겨찾기 모듈로 보내는 메시지를 저장
     public static String replybookmark_msg="즐겨찾기 모듈의 응답메시지가 저장되지 않았습니다"; //즐겨찾기 모듈이 요청을 보냈던 클라이언트에게 보내는 메시지를 저장
+    public static Socket findtour_socket;
+    public static Socket findtour_client_socket;
+    public static String tofindtour_msg;
+    public static String replyfindtour_msg;
 
 
 }
