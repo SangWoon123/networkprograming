d161bc5732df38c6eb8586ec59e97b2af59bd8a1
DongHyeon <100687971+rlagus720@users.noreply.github.com>
2023-06-05 02:15:16 +0900

    Add files via upload

---
+++ src/client/Main.java
@@ -0,0 +1,29 @@
+package client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.rmi.UnknownHostException;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		try {
+			Socket c_socket = new Socket("127.0.0.1", 9398);
+			
+			System.out.println("dkdkdk");
+			
+			ReceiveThread rec_thread = new ReceiveThread();
+			rec_thread.setSocket(c_socket);
+			
+			SendThread send_thread = new SendThread();
+			send_thread.setSocket(c_socket);
+			
+			send_thread.start();
+			rec_thread.start();
+		} catch(UnknownHostException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
