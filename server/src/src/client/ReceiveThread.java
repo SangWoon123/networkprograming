ccc2487286876704ed48e7c1b5353b0bbbfa324f
DongHyeon <100687971+rlagus720@users.noreply.github.com>
2023-06-05 02:20:39 +0900

    Add files via upload

---
+++ server/src/src/client/ReceiveThread.java
@@ -0,0 +1,43 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import geoLocation.geoLocation;
+import Tour.TourAPI;
+
+public class ReceiveThread extends Thread {
+	
+	private static final geoLocation Geo_Location = new geoLocation();
+	private static final TourAPI TourAPI = new TourAPI();
+	public static String TourData = "";
+	
+	private Socket m_Socket;
+	
+	public void run() {
+		
+		super.run();
+		
+		try {
+			BufferedReader tmpbuf = new BufferedReader(new InputStreamReader(m_Socket.getInputStream(), "UTF-8"));
+			
+			String receiveString;
+			String[] split;
+			
+			while(true) {
+				receiveString = tmpbuf.readLine();
+				split = receiveString.split(">");
+				if(split.length >= 2 && split[0].equals("tourList")) {
+					TourData = TourAPI.TourInfor(Geo_Location.GeoLocation(split[1]));
+				}
+			}
+		} catch(IOException e) {	
+		}
+	}
+	
+	public void setSocket(Socket _socket) {
+		m_Socket = _socket;
+	}
+}
