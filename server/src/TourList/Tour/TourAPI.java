f02c787724e78a792357611175c886451b1556a3
DongHyeon <100687971+rlagus720@users.noreply.github.com>
2023-06-07 00:21:17 +0900

    Add files via upload

--- server/src/TourList/Tour/TourAPI.java
+++ server/src/TourList/Tour/TourAPI.java
@@ -73,7 +73,7 @@ public class TourAPI {
                     String title = tourInfor.get("title").toString();
 
                     ti.append(",주소1 : " + addr1 + " ");
-                    ti.append("거리 : " + dist + " ");
+                    ti.append("거리 : " + dist + "km ");
                     ti.append("전화번호 : " + tel + " ");
                     ti.append("관광지명 : " + title + " ");
                 }
